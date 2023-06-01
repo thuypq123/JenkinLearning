e579db584ca71bf81b92cb40c13cace2bb5893a5
lmthuy37 <lmthuy37@gmail.com>
2023-06-01 09:06:53 +0700

    first commit

---
+++ LearningJenkin/src/main/java/com/example/learningjenkin/LearningJenkinApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningjenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningJenkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningJenkinApplication.class, args);
+    }
+
+}
---
+++ LearningJenkin/src/test/java/com/example/learningjenkin/LearningJenkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningjenkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJenkinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
